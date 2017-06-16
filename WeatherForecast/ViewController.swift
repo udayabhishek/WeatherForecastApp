ad2e071c08e6a3c7dcd122796cb919b93ec36f75
Uday Kumar Abhishek - (Digital) <ud341583@l-156088444.local>
2017-06-16 16:56:55 +0530

    Initial Commit

---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Uday Kumar Abhishek - (Digital) on 16/06/17.
+//  Copyright © 2017 Uday Kumar Abhishek - (Digital). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
